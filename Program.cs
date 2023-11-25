3152a002e78e29b254454b1fc67b17d08124533e
Jakub Zięba <jakubzieba7@gmail.com>
2023-11-25 17:19:29 +0100

    Injected UnitOfWork and initialized in controller

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using MyFinances.Models;
 using MyFinances.Models.Domains;
 
 namespace MyFinances
@@ -9,6 +10,8 @@ namespace MyFinances
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<UnitOfWork, UnitOfWork>();
 
             // Add services to the container.
 
